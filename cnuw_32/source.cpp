67d7bfd28cbcd40438e96d9d42a42c9853cfa828
Hatsune Miku <eggtart@mikutart.com>
2018-06-20 16:26:56 +0800

    temp

--- cnuw_32/source.cpp
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-int main() {
-	"miku!";
-	double cost, paid;
-	scanf("%lf%lf", &cost, &paid);
-	printf("change=%.2lf", paid - cost);
-	return 0;
-}
